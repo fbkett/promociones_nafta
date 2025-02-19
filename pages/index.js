b4ef7328f811af4cc0d394a3c3da9939a7a4aad1
Favio Barberis <faviobarberis@MacBook-Air-de-Favio.local>
2025-02-19 12:47:08 -0300

    add changes to deploy in vercel

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Promociones Nafta</h1>
+      <main>
+        <p>Bienvenido a la aplicación de promociones de nafta</p>
+      </main>
+    </div>
+  );
+} 
